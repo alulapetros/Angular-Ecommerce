3f90e8c5fb6cfec7a68b04892bbc7edb6a0248b4
Alula Mekonen <80268336+alulapetros@users.noreply.github.com>
2022-08-13 23:25:24 -0400

    App is Deployed

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+// //Install express server
+// const express = require('express');
+// const path = require('path');
 
-const app = express();
+// const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/Angular-Ecommerce'));
+// // Serve only the static files form the dist directory
+// app.use(express.static('./dist/Angular-Ecommerce'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/Angular-Ecommerce/'}),
-);
+// app.get('/*', (req, res) =>
+//     res.sendFile('index.html', {root: 'dist/Angular-Ecommerce/'}),
+// );
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
